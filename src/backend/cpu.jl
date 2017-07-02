978cb0550b27f148140aa18f8be6a64c198d63d4
Mike J Innes <mike.j.innes@gmail.com>
2017-07-02 21:31:39 +0100

    basic cpu codegen

--- src/backend/cpu.jl
+++ src/backend/cpu.jl
@@ -1,27 +1,52 @@
-# Remove lambdas
+domainm(idx, vars) = :(1:size($(vars[idx[1]]), $(idx[2])))
 
-interpid(ctx::Context, f, args...) = vertex(f, args...)
+interpid(ctx::Context, f, args...) = vertex(f, DataFlow.constant.(args)...)
+
+ndloop(iters, body) =
+  Expr(:for,
+       Expr(:block, [:($i = $xs) for (i, xs) in iters]...),
+       body)
+
+function ireduction(_, ctx::Context, ::typeof(reduce), red, v0, v)
+  λ, f, vars = value(v), v.inputs[1], interpret.(ctx, v.inputs[2:end])
+  xs = map(i -> domainm(i, DataFlow.syntax.(vars)), ctx[:lamdbas][λ])
+  is = [gensym(:i) for _ in xs]
+  body = DataFlow.syntax(interpret(ctx, v)(is...))
+  quote
+    sum = $(DataFlow.syntax(v0))
+    $(ndloop(zip(is, xs), :(sum += $(body))))
+    sum
+  end |> DataFlow.constant
+end
+
+ireduction(f, ctx::Context, a...) = f(ctx, a...)
 
 function inline(v::IVertex, is, args...)
-  ctx = Context(mux(ilambda, iargs, ituple, interpid))
-  interpret(ctx, v, args...)(is...) |> DataFlow.striplines
+  arrow, lambdas = infer_(v)
+  ctx = Context(mux(iline, ireduction, ilambda, iargs, ituple, interpid),
+                lamdbas = lambdas)
+  out = interpret(ctx, v, args...)
+  isempty(arrow.ts[end]) ? out : out(is...)
 end
 
 inline(f::Func, args...) = inline(f.graph, args...)
-
-# Skeleton
 
 domains(a::Arrow) = map(d -> subidx(d, a.ts[1:end]), a.ts[end])
 
 function cpu(f::Func)
   args = [gensym() for _ = 1:DataFlow.graphinputs(f.graph)]
   typ = infer(f)
-  doms = domains(typ)
-  is = [:i, :j, :k][1:length(typ.ts[end])]
-  iters = [:($i = 1:size($(args[d[1]]), $(d[2]))) for (i, d) in zip(is, doms)]
-  :(function (out, $(args...))
-      $(Expr(:for, :($(iters...);), quote
-        out[$(is...)] = $(DataFlow.syntax(inline(f, (is...,), args...)))
-      end))
-    end)
+  if isempty(typ.ts[end])
+    :(function ($(args...),)
+        $(DataFlow.syntax(inline(f, [:i], args...)))
+      end)
+  else
+    doms = map(i -> domainm(i, args), domains(typ))
+    is = [gensym(:i) for _ in doms]
+    :(function (out, $(args...),)
+        $(ndloop(zip(is, doms),
+            :(out[$(is...)] = $(DataFlow.syntax(inline(f, is, args...))))))
+        return out
+      end)
+  end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Tokamak: @tk, infer
+using Tokamak: @tk, infer, cpu
 using Base.Test
 
 @tk diag(A)[i] = A[i,i]
@@ -12,3 +12,16 @@ using Base.Test
 @test string(infer(add)) == "(m) → (m) → (m)"
 @test string(infer(sum)) == "(m) → ()"
 @test string(infer(mul)) == "(m, n) → (n, o) → (m, o)"
+
+diagf = eval(cpu(diag))
+@test diagf(zeros(3), reshape(1:9, (3, 3))) == [1,5,9]
+
+sumf = eval(cpu(sum))
+@test sumf([1,2,3]) == Base.sum([1,2,3])
+
+addf = eval(cpu(add))
+@test addf(zeros(3), [1,2,3],[4,5,6]) == [5,7,9]
+
+mulf = eval(cpu(mul))
+A, B = rand(5,5), rand(5,5)
+@test mulf(zeros(5,5), A, B) ≈ A*B
