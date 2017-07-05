9996cbd70467dc5a76d167c9b36ade86ca32583b
Mike J Innes <mike.j.innes@gmail.com>
2017-07-05 14:50:04 +0100

    nicer func printing

--- src/syntax.jl
+++ src/syntax.jl
@@ -12,11 +12,14 @@ end
 graphm(args, body) = DataFlow.graphm(MacroTools.flatten(body), args = args)
 
 struct Func
+  name::Symbol
   graph::IVertex{Any}
 end
+
+Base.show(io::IO, f::Func) = print(io, f.name, "::Func")
 
 macro tk(ex)
   @capture(shortdef(desugar(ex)), f_(args__) = body_)
   ex = esc(DataFlow.constructor(graphm(args, body)))
-  :($(esc(f)) = Func($ex); nothing)
+  :($(esc(f)) = Func($(Expr(:quote, f)), $ex); nothing)
 end
