ac9453ae032d4ae35d764dab976e96ab74714d0e
Mike J Innes <mike.j.innes@gmail.com>
2017-07-06 01:26:50 +0100

    real inlining

--- src/inline.jl
+++ src/inline.jl
@@ -1,16 +1,25 @@
-using DataFlow.Interpreter
-
-inline(ctx::Context, f, args...) = vertex(f, DataFlow.constant.(args)...)
-
-function inline(ctx::Context, f::Func, args...)
-  interpret(ctx, f.graph, args...)
+function inlinef(v::IVertex)
+  prewalk(v) do v
+    isa(v.value, Func) ? DataFlow.spliceinputs(inlinef(v.value.graph), v.inputs...) :
+      v
+  end
 end
 
-function inline(ctx::Context, λ::DataFlow.Lambda, vars...)
-  vertex(DataFlow.Lambda(interpv(ctx, λ.body)), vars...)
+function fuse(v::IVertex)
+  prewalk(inlinef(v)) do v
+    isa(v.value, DataFlow.Lambda) || return v
+    v = DataFlow.fuse(v)
+    v.value = DataFlow.Lambda(v.value.args, fuse(v.value.body))
+    return v
+  end |> DataFlow.detuple
 end
 
-function inline(v::IVertex)
-  ctx = Context(mux(iline, iargs, iconst, ituple, inline))
-  out = interpv(ctx, v)
+function inlinea(v::IVertex)
+  prewalk(v) do v
+    isa(v.value, DataFlow.Lambda) && return vertex(DataFlow.Lambda(v.value.args, inlinea(v.value.body)), v.inputs...)
+    v.value == getindex && v[1].value isa DataFlow.Lambda || return v
+    DataFlow.spliceinputs(v[1].value.body, v[1].inputs..., v.inputs[2:end]...) |> DataFlow.detuple
+  end
 end
+
+inline(v::IVertex) = v |> fuse |> inlinea
