935da1f6d3f5fea76bda31ffd1b73824df260e74
Mike J Innes <mike.j.innes@gmail.com>
2017-10-11 23:09:08 +0100

    this is useless

--- src/Stencil/Stencil.jl
+++ src/Stencil/Stencil.jl
@@ -5,7 +5,6 @@ using DataFlow: Lambda, OLambda, prewalk, postwalk, isconstant, value, inputnode
 
 include("syntax.jl")
 include("inference.jl")
-include("schedule.jl")
 include("inline.jl")
 include("backend/cpu.jl")
 
--- src/Stencil/schedule.jl
+++
@@ -1,1 +0,0 @@
-function store end
