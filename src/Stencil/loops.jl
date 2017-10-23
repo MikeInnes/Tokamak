cd73bfa356f1d3a40c03022b599dd127aab15a61
Mike J Innes <mike.j.innes@gmail.com>
2017-10-23 18:49:16 +0100

    loop printing

--- src/Stencil/loops.jl
+++ src/Stencil/loops.jl
@@ -1,1 +1,14 @@
+using MacroTools: @q
+
 struct Loop end
+
+function DataFlow.toexpr(f::Loop, λ)
+  exs = MacroTools.block(λ).args
+  @assert @capture(exs[end], (args__,) -> body_)
+  iters = Expr(:block, args...)
+  loop = Expr(:for, iters, body)
+  @q begin
+    $(exs[1:end-1]...)
+    $loop
+  end
+end
