d961e9521f7e1a8d0d61c7ec5b837795f3f5f08f
Mike J Innes <mike.j.innes@gmail.com>
2018-03-22 20:22:17 -0700

    cpu tests

--- src/codegen/cpu.jl
+++ src/codegen/cpu.jl
@@ -1,5 +1,7 @@
-function compile(f::Func, ts...)
+function cpuv(f::Func, ts...)
   ar, v = infer(f, ts...)
   v = insert_domains(striptypes(v), ar)
   v = tolambda(v, length(ts))
 end
+
+cpu(a...) = cpuv(a...) |> syntax |> eval
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
-using Tokamak
+using Tokamak: @tk, cpu, dagger
 using Base.Test
 
 @testset "Tokamak" begin
+
+@tk diag(A)[i] = A[i,i]
+@tk outer(x,y)[i,j] = x[i]*y[j]
+@tk trans(A)[i,j] = A[j,i]
+@tk add(A,B)[i] = A[i] + B[i]
+
+@testset "CPU" begin
+
+A = rand(5,5)
+B = rand(5,5)
+x = rand(5)
+y = rand(5)
+
+cpu_diag = cpu(diag, Matrix{Float64})
+@test cpu_diag(A) == Base.diag(A)
+
+cpu_outer = cpu(outer, Vector{Float64}, Vector{Float64})
+@test cpu_outer(x,y) == x*y'
+
+cpu_trans = cpu(trans, Matrix{Float64})
+@test cpu_trans(A) == A'
+
+cpu_add = cpu(add, Vector{Float64}, Vector{Float64})
+@test cpu_add(x,y) == x+y
+
+end
 
 end
